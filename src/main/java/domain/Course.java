016cd31bebb8c28e4d830f503f6ad00ce086fef2
MichaelBEspinoza <michaelbonilla0209@gmail.com>
2024-06-08 16:59:39 -0600

    Course Operations 100% funcional (Utiliza un archivo .dat) A la espera de la conexion con la interfaz

--- src/main/java/domain/Course.java
+++ src/main/java/domain/Course.java
@@ -1,8 +1,11 @@
 package domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Course {
+public class Course implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private int id; /**Identificador del curso**/
 
     private String name; /**Nombre del curso**/
