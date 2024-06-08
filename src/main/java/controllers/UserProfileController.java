3daeb67176023f215c8df1618a94b4d64e21d6da
Katherine <kfbrenes14@gmail.com>
2024-06-07 18:54:37 -0600

    Avances de la clase de sistema de seguridad pt1

---
+++ src/main/java/controllers/UserProfileController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
+public class UserProfileController {
+    @javafx.fxml.FXML
+    private ImageView imageView;
+    @javafx.fxml.FXML
+    private Circle circleImage;
+
+    @javafx.fxml.FXML
+    public void mainPageOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void editProfileOnAction(ActionEvent actionEvent) {
+    }
+}
