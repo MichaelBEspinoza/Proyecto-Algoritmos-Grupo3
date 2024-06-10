0e69ef3d937cfb6cac7501b180162442f547d280
Katherine <kfbrenes14@gmail.com>
2024-06-09 21:37:51 -0600

    Avances con interfaces de la sección de cursos

---
+++ src/main/java/controllers/InscriptionCourseController.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+public class InscriptionCourseController {
+    @javafx.fxml.FXML
+    private Menu menuPaginaPrincipal;
+    @javafx.fxml.FXML
+    private Menu menuAyuda;
+    @javafx.fxml.FXML
+    private TextField txf_inscripcionPassword;
+    @javafx.fxml.FXML
+    private TextField txf_courseID;
+    @javafx.fxml.FXML
+    private MenuItem mn_mainPage;
+    @javafx.fxml.FXML
+    private TextField txf_name;
+    @javafx.fxml.FXML
+    private Menu menuCursos;
+    @javafx.fxml.FXML
+    private MenuItem mn_courses;
+
+    @javafx.fxml.FXML
+    public void ayudaOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void pagePrincipalOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cursosOnAction(ActionEvent actionEvent) {
+    }
+}
