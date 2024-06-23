8442962d19e80486a32d3a108cf6ac7eefaa9677
Katherine <kfbrenes14@gmail.com>
2024-06-22 21:52:44 -0600

    Interfaces de reportes y bitácora hecha por Alejandro

--- src/main/java/controllers/StudentCourseEvaluationController.java
+++ src/main/java/controllers/StudentCourseEvaluationController.java
@@ -1,10 +1,15 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
+import ucr.proyecto.proyectoalgoritmosv1.HelloApplication;
+
+import java.io.IOException;
 
 public class StudentCourseEvaluationController {
 
@@ -24,19 +29,34 @@ public class StudentCourseEvaluationController {
     @javafx.fxml.FXML
     private MenuItem mn_courses;
 
-    @javafx.fxml.FXML
-    public void ayudaOnAction(ActionEvent actionEvent) {
+    private void loadPage(String page) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(page));
+        try {
+            this.bp.setCenter(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @javafx.fxml.FXML
+    @FXML
+    public void ayudaOnAction(ActionEvent actionEvent) {
+        bp.getChildren().clear();
+        loadPage("usersupport.fxml");
+    }
+
+    @FXML
     public void pagePrincipalOnAction(ActionEvent actionEvent) {
+        bp.getChildren().clear();
+        loadPage("mainPage.fxml");
     }
 
     @javafx.fxml.FXML
     public void addOnAction(ActionEvent actionEvent) {
     }
 
-    @javafx.fxml.FXML
+    @FXML
     public void cursosOnAction(ActionEvent actionEvent) {
+        bp.getChildren().clear();
+        loadPage("userCourses.fxml");
     }
 }
